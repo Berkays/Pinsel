e34f704e2628eea9b2fd5fff960af9a35c64f2c1
Berkay GURSOY <berkaygursoy@gmail.com>
2019-03-25 00:57:14 +0300

    watch error handling

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var minify = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 const image = require('gulp-image');
 var browserify = require('browserify');
 var rename = require('gulp-rename');
@@ -32,8 +33,6 @@ const moved_assets = [
     './public/src/**/*.json'
 ];
 
-const watched = Object.values(compiled_assets);
-
 const _move = () => {
     return gulp.src(moved_assets, { base: base_path })
         .pipe(cache('move'))
@@ -41,29 +40,30 @@ const _move = () => {
 };
 
 const _js = (done) => {
-    return glob(compiled_assets.js, { matchBase: false}, function (err, files) {
+    return glob(compiled_assets.js, { matchBase: false }, function (err, files) {
         if (err) done(err);
 
-        var tasks = files.map(function(entry) {
-        var dir = path.relative(base_path,entry);
-        dir = path.dirname(dir);
+        var tasks = files.map(function (entry) {
+            var dir = path.relative(base_path, entry);
+            dir = path.dirname(dir);
 
-        return browserify({ entries: [entry] })
-            .bundle()
-            .pipe(cache('js'))
-            .pipe(source(entry))
-            .pipe(rename({ dirname: dir }))
-            .pipe(gulp.dest(dist_path));
+            return browserify({ entries: [entry] })
+                .bundle().on('error', (error) => console.error(error))
+                .pipe(source(entry))
+                .pipe(cache('js'))
+                //.pipe(streamify(uglify()))
+                .pipe(rename({ dirname: dir }))
+                .pipe(gulp.dest(dist_path));
         });
-    es.merge(tasks).on('end',done);
+        es.merge(tasks).on('end', done);
     });
 };
 
 const _less = () => {
     return gulp.src(compiled_assets.less_css, { base: base_path })
         .pipe(cache('less'))
-        .pipe(less()) // compile to css
-        .pipe(minify()) // minify
+        .pipe(less()).on('error', (error) => console.error(error)) // compile to css
+        .pipe(minify()).on('error', (error) => console.error(error)) // minify
         .pipe(gulp.dest(dist_path)) // output
         .pipe(browserSync.reload({ stream: true })); // reload on change
 };
@@ -71,7 +71,7 @@ const _less = () => {
 const _css = () => {
     return gulp.src(compiled_assets.css, { base: base_path })
         .pipe(cache('css'))
-        .pipe(minify()) // minify
+        .pipe(minify()).on('error', (error) => console.error(error)) // minify
         .pipe(gulp.dest(dist_path)) // output
         .pipe(browserSync.reload({ stream: true })); // reload on change
 };
@@ -79,26 +79,32 @@ const _css = () => {
 const _image = () => {
     return gulp.src(compiled_assets.images, { base: base_path })
         .pipe(cache('image'))
-        .pipe(image())
+        .pipe(image()).on('error', (error) => console.error(error))
         .pipe(gulp.dest(dist_path));
 };
 
-const _nodemon = () => {
+const _server = (cb) => {
+    var started = false;
     return nodemon({
         script: 'server.js',
+        ignore: ['!(server.js)'],
         env: { 'NODE_ENV': 'development' }
-    });
-};
+    }).on('start', () => {
+        if (!started) {
+            cb();
+            started = true;
 
-const _browsersync = () => {
-    browserSync.init(null, {
-        proxy: "http://localhost:3000",
-        files: [
-            // "public/dist/**/*.*",
-            "views/*.pug"],
-        port: 3001
+            browserSync.init(null, {
+                proxy: "http://localhost:3000",
+                files: [
+                    //"public/dist/**/*.*",
+                    "views/*.pug"],
+                port: 3001
+            });
+
+            gulp.watch('public/src/**/*.*', gulp.series('build'));
+        }
     });
-    gulp.watch(watched, gulp.parallel('build'));
 };
 
 const _clean = () => {
@@ -108,4 +114,4 @@ const _clean = () => {
 
 gulp.task('clean', gulp.series(_clean));
 gulp.task('build', gulp.parallel(_js, _less, _css, _image, _move));
-gulp.task('sync', gulp.series(_clean, 'build', gulp.parallel(_nodemon, _browsersync)));
+gulp.task('sync', _server);
