b2b3b7a793f140933bb07a65dc54b7a243610d18
Berkay GURSOY <berkaygursoy@gmail.com>
2019-03-31 13:20:22 +0300

    activeNav navbar data

--- routes/gallery.js
+++ routes/gallery.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Browser page. */
 router.get('/', function (req, res, next) {
-    res.render('gallery', { title: 'Browse Art Gallery' });
+    res.render('gallery', { title: 'ArtChain Gallery', activeNav: 'browse' });
 });
 
 module.exports = router;
