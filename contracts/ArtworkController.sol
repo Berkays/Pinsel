5b4e0b016ba5e57082f7dd03492903f633586f42
Berkay GURSOY <berkaygursoy@gmail.com>
2019-04-02 12:07:43 +0300

    functions

--- contracts/ArtworkController.sol
+++ contracts/ArtworkController.sol
@@ -2,14 +2,44 @@ pragma solidity ^0.5;
 
 contract ArtworkController {
 
+    event Donated();
+
     struct Artwork {
         address hash;
         address owner;
-        uint averageSpent;
+        uint averageDonation;
         uint donationSum;
         uint donationCount;
+
+        bytes16 imageName;
     }
 
     mapping(address => Artwork) public artworks;
+    uint private artworkCount = 0;
+
+    constructor() public {
+    }
+
+    modifier inDonationLimit(address artworkId,uint amount)
+    {
+        uint min = artworks[artworkId].averageDonation - 5;
+        uint max = artworks[artworkId].averageDonation + 5;
+        if(min < 1)
+            min = 1;
+        if(max < 0) // Prevent Overflow
+            max = 1000;
+        require(amount > min && amount < max, "Donation amount should be in the limits of average artwork donation.");
+        _;
+    }
+
+
+    function donate(address receiver, uint amount) 
+        payable 
+        public 
+        inDonationLimit(receiver,amount)
+    {
+
+        emit Donated();
+    }
 
 }
