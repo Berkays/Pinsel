c6e6e20ecdfe60ec3df3b8a8fa1f37c1731ce6ab
Berkay GURSOY <berkaygursoy@gmail.com>
2019-04-22 22:06:14 +0300

    docker client connection

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '5777'
+    },
+    dev_docker: {
+        host: '127.0.0.1',
+        port: 7545,
+        network_id: '*'
     }
   }
 }
