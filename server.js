809b71ea23fbc428e24e6906adc9eb058c75f4d6
Berkay GURSOY <berkaygursoy@gmail.com>
2019-04-22 22:05:53 +0300

    ipfs connection

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
-var compression = require('compression')
+const compression = require('compression')
+const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const app = express();
 
-const Web3 = require('web3');
-const truffle_connect = require('./connection/app.js');
-
 const ipfsClient = require('ipfs-http-client');
-// leaving out the arguments will default to these values
-// or connect with multiaddr
 const ipfs = ipfsClient('/ip4/127.0.0.1/tcp/5001')
 
 app.ipfs = ipfs;
 
-ipfs.get('/ipfs/QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv/readme',(err,files) => {
-    console.log(files);
+let ipfsOperation = ipfs.get('/ipfs/QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv/readme');
+
+ipfsOperation
+.then((results) => {
+    console.log("IPFS Connected.");
+    app.ipfs.connected = true;
+})
+.catch((err) => {
+    app.ipfs.connected = false;
+    console.warn("IPFS Not Connected.");
 });
 
 const port = process.env.PORT || 3000;
@@ -28,9 +32,9 @@ var uploadRouter = require('./routes/upload');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
 app.use(express.json());
 app.use(compression());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(fileUpload());
@@ -39,12 +43,6 @@ app.use(express.static(path.join(__dirname, 'public', 'dist')));
 app.use('/', indexRouter);
 app.use('/gallery', galleryRouter);
 app.use('/upload', uploadRouter);
-
-// app.listen(port, () => {
-//     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-//     //truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-//     console.log("Express Listening at http://localhost:" + port);
-// });
 
 app.use(function (req, res, next) {
     next(createError(404));
