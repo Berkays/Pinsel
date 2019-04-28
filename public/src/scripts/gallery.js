ec066ac44016850b9252c365a08300bb92241f50
Berkay GURSOY <berkaygursoy@gmail.com>
2019-04-29 02:00:46 +0300

    transaction actions and fields

--- public/src/scripts/gallery.js
+++ public/src/scripts/gallery.js
@@ -1,12 +1,8 @@
-const ipfsClient = require('ipfs-http-client');
-const buffer = require('buffer');
-
 require('owl.carousel/dist/owl.carousel');
 
 const ipfsURL = 'http://127.0.0.1:8080/ipfs/';
 
 $(document).ready(async () => {
-    const ipfs = initIPFS();
     await App.init();
 
     const artworkHashes = await App.getArtworks(); // Return IPFS hashes of uploaded images.
@@ -27,7 +23,7 @@ function initSlideshow() {
         center: true,
         items: 3,
         loop: true,
-        margin: 200,
+        margin: 100,
         autoWidth: true,
         autoplay: true,
         autoplayTimeout: 5000,
@@ -43,8 +39,13 @@ function convertArtworkDetails(hash,details) {
     artName = web3.utils.hexToUtf8(details[0]);
     artAuthor = web3.utils.hexToUtf8(details[1]); 
     artDescription = web3.utils.hexToUtf8(details[2]); 
-    artAvgTransfer = details[4].toNumber(); 
-    artTransferCount = details[5].toNumber();
+
+    var d = new Date();
+    d.setTime(details[3].toNumber());
+    artUploadDate = d.toJSON().slice(0,10); 
+    artTransactionFee = details[4][0];
+    artAvgTransfer = web3.utils.fromWei(details[4][1],'ether'); 
+    artTransferLimit = parseInt(details[4][2]);
 
     artImage = ipfsURL + hash;
 
@@ -52,15 +53,19 @@ function convertArtworkDetails(hash,details) {
         name:artName,
         author:artAuthor,
         description:artName,
+        date: artUploadDate,
+        fee: artTransactionFee,
         avgTransfer:artAvgTransfer,
-        transferCount:artTransferCount,
-        image:artImage
+        transferLimit: artTransferLimit,
+        image:artImage,
+        hash: hash
     };
 
     return _details;
 }
 
 function addArtworkElement(hash,details) {
+    const $divContainer = $("<div>", { "class": "slide-img-container d-flex justify-content-center align-items-center" });
     const $div = $("<div>", { "class": "slide-img" });
     const $a = $("<a>", { "id": "modalTest", "data-toggle": "modal", "data-target": "#artModal", "data-artwork": JSON.stringify(details) });
     const $img = $("<img>", { "class": "rounded-lg shadow-lg", "src":"#" });
@@ -73,31 +78,55 @@ function addArtworkElement(hash,details) {
     $span.text(details.name);
     $i.text('From ' + details.author);
 
-    const $el = $div.append($a.append($img).append($span.append($br).append($i)));
+    const $el = $divContainer.append($div.append($a.append($img).append($span.append($br).append($i))));
     $('#featured').append($el);
 }
 
 $('#artModal').on('show.bs.modal', function (event) {
     var sender = $(event.relatedTarget); // Button that triggered the modal
-    console.log("clicked");
+
     var data = JSON.parse($(sender).attr('data-artwork'));
 
     var modal = $(this);
 
-    modal.find('#artModalTitle').text(data.name);
+    modal.find('.modal-title').text(data.name);
     modal.find('h2').text(data.name);
     modal.find('#artModalDescription').text(data.description);
-    modal.find('#artModalUploadDate').text("Uploaded At " + data.upload_date);
+    modal.find('#artModalUploadDate').text("Uploaded At " + data.date);
     modal.find('#artModalAuthor').text("By " + data.author);
     modal.find('#artModalImage').attr('src', data.image);
+    modal.find('#artModalLimit').html('Amount Left: <b> &nbsp;' + data.transferLimit + '</b>');
+    
+    var artModalFee = modal.find('#artModalFee');
+    var artModalInputFee = modal.find('#artModalInputFee');
+    var artModalAverage = modal.find('#artModalAverage');
 
+    const optionalFee = data.fee <= 0;
+    if(optionalFee)
+    {
+        //Optional Fee
+        artModalFee.hide();
+        artModalInputFee.show();
+        artModalAverage.show();
+
+        artModalAverage.html("Average Transaction: <b> &nbsp;" + (data.avgTransfer) + " Ether</b>");
+    }
+    else
+    {
+        artModalFee.show();
+        artModalInputFee.hide();
+        artModalAverage.hide();
+        
+
+        const etherValue = web3.utils.fromWei(data.fee,'ether');
+        artModalFee.html('Artwork Fee: <b> &nbsp;' + etherValue + '</b>');
+
+    }
 
     var minLimit = (data.total_donation / data.donation_count) - 5;
     if (minLimit < 0.1)
         minLimit = 0.1;
     var maxLimit = (data.total_donation / data.donation_count) + 5;
-
-    modal.find('#artModalAverageDonation').html("Average Transaction: <b>" + (data.total_donation / data.donation_count) + " Ether</b>");
 
     var donationSlider = modal.find('#donationSlider');
     var donationSliderLabel = $("label[for=donationSlider]");
@@ -115,25 +144,22 @@ $('#artModal').on('show.bs.modal', function (event) {
     newVal = donationSlider.val();
     donationSliderLabel.html("Transaction Amount: <b>" + newVal + " Ether</b>");
 
-    var sendDonation = modal.find('#sendDonation');
+    var sendBtn = modal.find('#applyTransactionBtn');
 
-    sendDonation.unbind('click').on('click', (e) => {
-        e.preventDefault();
-
-        var body = {
-            "name": data.name,
-            "donation": donationSlider.val()
-        };
-
-        $.ajax({
-            type: "POST",
-            url: "/gallery/donate",
-            data: body,
-            cache: false,
-            success: function (result) {
-                console.log("Success");
-            }
-        });
+    sendBtn.on('click', async () => {
+        let feeInput = 0;
+        if(optionalFee)
+        {
+            feeInput = artModalInputFee.children().val();
+            console.log(feeInput);
+        }
+        else
+        {
+            feeInput = data.fee;
+        }
+        const etherValue = web3.utils.toWei(feeInput, 'wei');
+        const result = await App.licenseArtwork(data.hash,etherValue);
+        console.log(result);
     });
 
 });
