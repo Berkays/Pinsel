a05b99fef259d992d19ede36fc94a1a5068ee251
Berkay GURSOY <berkaygursoy@gmail.com>
2019-04-30 20:52:50 +0300

    owned items list

--- public/src/scripts/account.js
+++ public/src/scripts/account.js
@@ -1,6 +1,8 @@
 require('particles.js');
 particlesJS.load('particles-js', '../config/particlesjs-config2.json');
 
+const ipfsURL = 'http://127.0.0.1:8080/ipfs/';
+
 $(document).ready(async () => {
 
     await App.init();
@@ -18,7 +20,26 @@ $(document).ready(async () => {
     if(ownedItems.length > 0)
     {
         $('#ownedItemStatus').hide();
+        populateList(ownedItems);
     }
     
     console.log(ownedItems);
 });
+
+function populateList(items) {
+    items.forEach(item => {
+        App.getArtworkDetails(item).then((details) => {
+            const $li = $("<li>", { "class": "list-group-item d-flex justify-content-start" });
+            const $img = $("<img>", { "class": "rounded shadow-sm" });
+            const $h1 = $("<h1>", { "class": "display-6 ml-3 my-auto" });
+            const $btn = $("<div>", { "class": "btn btn-light border border-dark ml-auto my-auto", "data-toggle": "modal", "data-target": "#artModal", "data-artwork":JSON.stringify(item) });
+            
+            $img.attr('src', ipfsURL + item);
+            $h1.text(web3.utils.hexToUtf8(details[0]));
+            $btn.text("View Details");
+            
+            const $el = $li.append($img).append($h1).append($btn);
+            $('#ownedItemList').append($el);
+        });
+    });
+};
