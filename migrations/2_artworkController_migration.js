562f2e4568b0f1d191a61e08d99bb83ee1c6aa3a
Berkay GURSOY <berkaygursoy@gmail.com>
2019-04-30 20:52:40 +0300

    account controller address parameter

--- migrations/2_artworkController_migration.js
+++ migrations/2_artworkController_migration.js
@@ -2,6 +2,7 @@ var ArtworkController = artifacts.require("./ArtworkController.sol");
 var AccountController = artifacts.require("./AccountController.sol");
 
 module.exports = function (deployer) {
+    console.log(deployer);
     deployer.deploy(AccountController).then(() => {
         return deployer.deploy(ArtworkController,AccountController.address);
     });
