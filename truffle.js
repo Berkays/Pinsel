9ebc26f6fa49685ca15bfb67416e55d7f436540f
Berkay GURSOY <berkaygursoy@gmail.com>
2019-05-08 21:36:14 +0300

    docker blockchain config

--- truffle.js
+++ truffle.js
@@ -7,8 +7,9 @@ module.exports = {
     },
     dev_docker: {
         host: '127.0.0.1',
-        port: 7545,
-        network_id: '*'
+        port: 8545,
+        network_id: '45',
+        from: '0xc7d45a6c84d0c40fe8c851a92f3dc88295fd9169'
     }
   }
 }
