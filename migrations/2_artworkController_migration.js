63d690f5db5c236db03aa3ab456c0edd1f17fc58
Berkay GURSOY <berkaygursoy@gmail.com>
2019-05-08 21:37:40 +0300

    contract builds

--- migrations/2_artworkController_migration.js
+++ migrations/2_artworkController_migration.js
@@ -1,9 +1,10 @@
 var ArtworkController = artifacts.require("./ArtworkController.sol");
 var AccountController = artifacts.require("./AccountController.sol");
 
+const adminAccount = "0xC7D45a6C84D0C40fE8c851A92f3Dc88295FD9169"; // Only this account can withdraw ArtworkController Funds.
+
 module.exports = function (deployer) {
-    console.log(deployer);
     deployer.deploy(AccountController).then(() => {
-        return deployer.deploy(ArtworkController,AccountController.address);
+        return deployer.deploy(ArtworkController, AccountController.address, adminAccount);
     });
 };
